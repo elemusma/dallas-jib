0a68684d20ad1c2148914d0347faaaa1ccdfb52d
Efrain Lemus <efraintlemusm@gmail.com>
2022-01-25 08:58:45 -0700

    first change after live

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -21,7 +21,7 @@ function Home() {
         <div className="container">
             <div className="row bg-faded-gold pt-5 pb-5 px-lg-5">
                 <div className="col-12 text-center pb-5">
-                    <h2>Recent Gallery</h2>
+                    <h2>Recent Gallery Testing changes getting pushed</h2>
                 </div>
 
 <div className="row px-lg-5">
